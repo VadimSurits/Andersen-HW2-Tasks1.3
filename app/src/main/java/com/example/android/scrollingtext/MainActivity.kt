262e4aee0775147a1c09f2e38b46eb34e3f62075
VadimSurits <sourits-moscow@mail.ru>
2021-09-27 16:25:08 +1000

    Task 1.3 finished

--- app/src/main/java/com/example/android/scrollingtext/MainActivity.kt
+++ app/src/main/java/com/example/android/scrollingtext/MainActivity.kt
@@ -9,3 +9,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//Ответы на вопросы из раздела 11 Homework:
+
+//Question 1
+//How many views can you use within a ScrollView? Choose one:
+//
+//Answer: One view or one view group
+
+//Question 2
+//Which XML attribute do you use in a LinearLayout to show views side by side? Choose one:
+//
+//Answer: android:orientation="horizontal"
+
+//Question 3
+//Which XML attribute do you use to define the width of the LinearLayout inside the scrolling view? Choose one:
+//
+//Answer: android:layout_width="wrap_content"
